eff9f128edb00ccfa9f80538c455c2b65c71791e
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-08-25 01:24:29 +0530

    Implement friend addition functionality with AddFriendForm and update FriendsList to display dynamic friends

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,51 @@ import "./App.css";
 import AddFriendForm from "./components/AddFriendForm";
 import FriendsList from "./components/FriendsList";
 import SplitBillForm from "./components/SplitBillForm";
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
 const App = () => {
-  const [addFriend,setAddFriend] = useState(false)
+  const [addFriend, setAddFriend] = useState(false);
+  const [friends, setFriends] = useState(initialFriends);
+
+  function handleAddFriend(newFriend) {
+    setFriends((friends) => [...friends, newFriend]);
+  }
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList addFriend={addFriend} setAddFriend={setAddFriend}/>
-        {addFriend && <AddFriendForm addFriend={addFriend} setAddFriend={setAddFriend}/>} 
+        <FriendsList
+          friends={friends}
+          addFriend={addFriend}
+          setAddFriend={setAddFriend}
+        />
+        {addFriend && (
+          <AddFriendForm
+            addFriend={addFriend}
+            setAddFriend={setAddFriend}
+            onAddFriend={handleAddFriend}
+          />
+        )}
       </div>
-      <SplitBillForm/>
+      <SplitBillForm />
     </div>
   );
 };
--- src/components/AddFriendForm.jsx
+++ src/components/AddFriendForm.jsx
@@ -1,21 +1,45 @@
 import "./../App.css";
-const AddFriendForm = ({ addFriend, setAddFriend }) => {
-  return (
-      addFriend && (
-        <div>
-        <form className="form-add-friend">
+import { useState } from "react";
+const AddFriendForm = ({ onAddFriend, addFriend, setAddFriend }) => {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48?u=118836");
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return;
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+    onAddFriend(newFriend);
+    setName("");
+    setImage("https://i.pravatar.cc/48?u=118836");
+  }
+  return ( 
+      <div>
+        <form onSubmit={handleSubmit} className="form-add-friend">
           <label>👥Friend name</label>
-          <input type="text" />
+          <input
+            value={name}
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+          />
           <label>🖼️Image URL</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+          />
           <button className="button">Add</button>
         </form>
 
         <button onClick={() => setAddFriend(!addFriend)} className="button">
           close
         </button>
-        </div>
-        )
+      </div>
   );
 };
 
