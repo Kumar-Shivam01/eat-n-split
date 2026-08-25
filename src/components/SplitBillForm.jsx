3f976f978e4bab2704af845108e71bd32d8af5a2
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-08-26 00:30:00 +0530

    Implement state management for bill splitting in SplitBillForm component

--- src/components/SplitBillForm.jsx
+++ src/components/SplitBillForm.jsx
@@ -1,20 +1,25 @@
+import { useState } from "react"
 
 const SplitBillForm = ({selectedFriend}) => {
+  const [bill,setBill] = useState('')
+  const [paidByUser,setPaidByUser] = useState('')
+  const friendBill = bill? bill-paidByUser: '';
+  const [whoIsPaying,setWhoIsPaying] = useState('user')
   return (
     <form className="form-split-bill">
         <h2>Split a bill with {selectedFriend.name}</h2>
 
         <label>💰 Bill value</label>
-        <input type="number"/>
+        <input type="number" value={bill} onChange={(e)=>setBill(Number(e.target.value))}/>
 
         <label>🙎 Your expense</label>
-        <input type="text" />
+        <input type="text" value={paidByUser} onChange={(e)=>setPaidByUser(Number(e.target.value)>bill?paidByUser:Number(e.target.value))}/>
 
         <label>🧑 {selectedFriend.name}'s expense</label>
-        <input type="number" disabled/>
+        <input type="number" value={friendBill} disabled/>
 
         <label>🤑 Who is paying the bill?</label>
-        <select>
+        <select value={whoIsPaying} onChange={(e)=>setWhoIsPaying(e.target.value)}>
             <option value="user">You</option>
             <option value="friend">X</option>
         </select>
